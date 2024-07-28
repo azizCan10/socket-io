c842d61c9df587b2d342bb8b331cab7be0785de3
Aziz Can Öz <oz.azizcann@gmail.com>
2024-07-28 15:28:19 +0300

    init

---
+++ src/main/java/com/test/socketIo/SocketIoApplication.java
@@ -0,0 +1,12 @@
+package com.test.socketIo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketIoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketIoApplication.class, args);
+    }
+}
